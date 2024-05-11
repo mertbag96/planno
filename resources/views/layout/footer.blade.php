df95d745eb7e94485e9712ffc8727ad0713346c4
Mert Bağ <bagmert1996@gmail.com>
2024-05-11 23:40:48 +0300

    Optimizing logo

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -5,10 +5,8 @@
         <div class="footer-logo">
 
             <a href="{{ route('index') }}" class="logo d-flex align-items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="#5d0cb0" width="32" height="32" class="me-2">
-                    <path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM240 320h-48v48c0 8.8-7.2 16-16 16h-32c-8.8 0-16-7.2-16-16V144c0-8.8 7.2-16 16-16h96c52.9 0 96 43.1 96 96s-43.1 96-96 96zm0-128h-48v64h48c17.6 0 32-14.4 32-32s-14.4-32-32-32z"/>
-                </svg>
-                Planno
+                <i class="fa-solid fa-parking fs-3 me-2"></i>
+                <span>Planno</span>
             </a>
 
             <p>
