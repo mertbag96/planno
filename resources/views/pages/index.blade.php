1322ba7daf1486a240910ad996e02e59b8345225
Mert Bağ <bagmert1996@gmail.com>
2024-05-12 14:44:40 +0300

    Update Home Page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,50 @@
 
 @section('content')
 
-    <h4>Index Page</h4>
+    <section class="banner">
+
+        <div class="banner-content container">
+
+            <h1>Plan your day, save your time!</h1>
+
+            <p>
+                Organize your work, become focused and organized in your daily life!
+            </p>
+
+            <a href="{{ route('auth.login') }}" class="banner-button">
+                Start for free
+            </a>
+
+        </div>
+
+    </section>
+
+    <section class="partners">
+
+        <div class="partners-content container">
+
+            <h3>
+                Our Partners
+            </h3>
+
+            <p>
+                10+ thousand people and teams trust their sanity and productivity to Planno
+            </p>
+
+            <img src="{{ asset('assets/images/partners/microsoft.png') }}" alt="Microsoft">
+
+            <img src="{{ asset('assets/images/partners/disney.png') }}" alt="Disney">
+
+            <img src="{{ asset('assets/images/partners/amazon.png') }}" alt="Amazon">
+
+            <img src="{{ asset('assets/images/partners/netflix.png') }}" alt="Netflix">
+
+            <img src="{{ asset('assets/images/partners/adobe.png') }}" alt="Adobe">
+
+            <img src="{{ asset('assets/images/partners/salesforce.png') }}" alt="Salesforce">
+
+        </div>
+
+    </section>
 
 @endsection
